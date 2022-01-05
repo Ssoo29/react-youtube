bf3f5ed576805020b001fa51f7ddabe568143760
Sungsoo <45908591+Ssoo29@users.noreply.github.com>
2022-01-06 08:36:40 +0900

    show video title list

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,26 @@
-import './app.css';
+import "./app.css";
+import { useState, useEffect } from "react";
+import VideoList from "./components/video_list/video_list";
 
 function App() {
-  return (
-    <h1>Hello :</h1>
-  );
+  const [videos, setVideos] = useState([]);
+  const [name, setName] = useState("elllie");
+
+  useEffect(() => {
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+
+    fetch(
+      "https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResult=25&key=AIzaSyBAcsikNGwrKYDhQuyL-2juSbD5tBFS2DE",
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => setVideos(result.items))
+      .catch((error) => console.log("error", error));
+  }, [name, videos]);
+  return <VideoList videos={videos}></VideoList>;
 }
 
 export default App;
