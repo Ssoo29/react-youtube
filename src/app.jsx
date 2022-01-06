11f66a62c53b101275aeab7580e45b89b944912a
Sungsoo <45908591+Ssoo29@users.noreply.github.com>
2022-01-06 16:47:56 +0900

    make video item

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,7 @@
 import "./app.css";
 import { useState, useEffect } from "react";
 import VideoList from "./components/video_list/video_list";
+import data from "./mockData/mostPopular";
 
 function App() {
   const [videos, setVideos] = useState([]);
@@ -16,9 +17,18 @@ function App() {
       "https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResult=25&key=AIzaSyBAcsikNGwrKYDhQuyL-2juSbD5tBFS2DE",
       requestOptions
     )
-      .then((response) => response.json())
-      .then((result) => setVideos(result.items))
-      .catch((error) => console.log("error", error));
+      .then((response) => {
+        if (response.status === 403) {
+          console.error("API key exceeded: ", response);
+          setVideos(data.items);
+        } else if(response.status === 200){
+          return response.json();
+        } else {
+          throw new Error("Unexpected Http Status Code");
+        }
+      })
+      // .then((responseJson) => setVideos(responseJson.items))
+      .catch((error) => console.error("error: ", error));
   }, [name, videos]);
   return <VideoList videos={videos}></VideoList>;
 }
